353fedf0ce2043ac12df0a6e8b7d99531fd0bde1
isaac7117 <eslash013@gmail.com>
2025-03-04 14:08:22 -0600

    Problema de bucle en urls.py correguido

--- usuarios/models.py
+++ usuarios/models.py
@@ -2,9 +2,11 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class Usuario(AbstractUser):
+    rol = models.CharField(max_length=50, default='usuario')
     ROLES = (
         ('admin', 'Administrador'),
         ('editor', 'Editor'),
         ('usuario', 'Usuario'),
+        
     )
     rol = models.CharField(max_length=20, choices=ROLES, default='usuario')
